e545608685d5ceeff9e2f3eb5bf2534a6e4276d4
Subrato Mondal <subratomondal374@gmail.com>
2024-09-10 21:39:36 +0530

    control flow added

---
+++ chipher_code/controlFlow.java
@@ -0,0 +1,44 @@
+// class Student{
+//     String name;
+//     int marks;
+//     boolean result(){
+//         if (marks>40){
+//             return true;
+//         } 
+//         else{
+//             return false;
+//         }
+//        }
+
+
+// }
+
+// class MaxNo{
+//     int findMax(int a,int b , int c){
+//         if(a>b && a>c){
+//             return a;
+//         }
+//         else if(b>a&&b>c){
+//             return b;
+//         }
+//         else{
+//             return c;
+//         }
+//     }
+// }
+
+// public class controlFlow{
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+//         s1.name = "Rakesh";
+//         s1.marks=45;
+//         Student s2=new Student();
+//         s2.name="ansh";
+//         s2.marks=35;
+//         System.out.println(s1.result());
+//         System.out.println(s2.result());
+//         MaxNo mx = new MaxNo();
+//         System.out.println(mx.findMax(20, 4, 18));
+
+//     }
+// }
